71dbbeaf13bad3fe511b263ee284c57a8b686e75
BuxTwo <arseniyarkhipov05@gmail.com>
2025-09-09 19:06:57 +0000

    First

---
+++ .config/nvim/lua/core/configs.lua
@@ -0,0 +1,19 @@
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.opt.showmode = true
+vim.wo.signcolumn = "yes"
+vim.opt.scrolloff = 8
+vim.opt.wrap = false
+vim.wo.linebreak = true
+vim.opt.virtualedit = "block"
+vim.opt.undofile = false
+vim.opt.shell = "/bin/bash"
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
